9a7b7bc635e02b966f35602e6966032912e01717
ketlyngomes212 <ketlyngomes212@gmail.com>
2026-08-17 23:36:31 -0300

    Html do login,

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Cadastro } from './cadastro/cadastro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'cadastro',
+    component: Cadastro
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Cadastro } from './cadastro/cadastro';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Cadastro],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('hello-world');
 }
