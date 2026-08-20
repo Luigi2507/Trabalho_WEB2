1580396b2e7db2f0f0f39d7de051bf3a5b4b803e
João Kretzschmar <134546778+kretzo@users.noreply.github.com>
2026-08-20 17:56:04 -0300

    animação do botão de cadastro

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -6,4 +6,27 @@ import { Component } from '@angular/core';
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
-export class Cadastro {}
+
+export class Cadastro {
+  public carregandoDados: boolean = false;
+  public bloqueado: boolean = false;
+  public btnCadastroTexto: string = 'Cadastrar-se';
+
+  public btnCadastroAnimacao(): void{
+    this.carregandoDados = true;
+    this.bloqueado = true;
+    this.btnCadastroTexto = 'Cadastro em andamento...';
+
+    setTimeout(() =>{
+      this.carregandoDados = false;
+      this.bloqueado = false;
+      this.btnCadastroTexto = 'Cadastrar-se';
+    },2000);
+  }
+
+  public resetBtnCadastro(): void{
+    this.carregandoDados = false;
+    this.bloqueado = false;
+    this.btnCadastroTexto = 'Cadastrar-se';
+  }
+}
