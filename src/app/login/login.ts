bd4f85e250704b152011b6275c4c327a9c43f1ac
João Kretzschmar <134546778+kretzo@users.noreply.github.com>
2026-08-20 17:28:57 -0300

    animação do botão de login

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -6,6 +6,27 @@ import { Component } from '@angular/core';
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
 
+export class Login {
+  public btnCarregando: boolean = false;
+  public btnTexto: string = 'Entrar';
+  public btnDesativado: boolean = false;
+
+  public btnLoginAnimacao(): void{
+    this.btnCarregando = true;
+    this.btnDesativado = true;
+    this.btnTexto = 'Aguarde...';
+
+    setTimeout(() => {
+    this.btnCarregando = false;
+    this.btnDesativado = false;
+    this.btnTexto = 'Entrar';
+    },2000);
+  }
+  
+  public resetBtnEstado (): void{
+    this.btnCarregando = false;
+    this.btnDesativado = false;
+    this.btnTexto = 'Entrar';
+  }
 }
