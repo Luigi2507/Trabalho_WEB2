060246e48ab18acb4b1c73149d7f628325023348
Luigi2507 <luigif2507@gmail.com>
2026-08-28 15:43:26 -0300

    Delete src/app/solicitacoes directory

--- src/app/solicitacoes/solicitacoes.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-solicitacoes',
-  imports: [],
-  templateUrl: './solicitacoes.html',
-  styleUrl: './solicitacoes.css',
-})
-export class Solicitacoes {}
