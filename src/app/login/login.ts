4b05df9497a889c1029ee672872b091ece7969a5
Daniela Cristina Amorim de Souza <danitina.souza@gmail.com>
2026-08-29 18:48:02 -0300

    alterações tela de login

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -13,6 +13,15 @@ export class Login {
   public btnLoginTexto: string = 'Entrar';
   public btnDesativado: boolean = false;
 
+  public bolinhas = Array.from({ length: 50 }, () => ({
+    top: Math.random() * 100,
+    left: Math.random() * 100,
+    tamanho: Math.random() * 20 + 8,
+    delay: Math.random() * 5,
+    duracao: Math.random() * 4 + 4,
+    cor: ['#db5b16', '#f5822b', '#ffb17a', '#c22e08'][Math.floor(Math.random() * 4)]
+  }));
+
   public btnLoginAnimacao(): void{
     this.btnCarregando = true;
     this.btnDesativado = true;
