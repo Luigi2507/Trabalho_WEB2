3188ddae4f105b133b3774047d2ee24f438b1831
Daniela Cristina Amorim de Souza <danitina.souza@gmail.com>
2026-08-31 23:39:59 -0300

    ajustes home

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,6 +2,18 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+interface Servico {
+  icone: string;
+  titulo: string;
+  descricao: string;
+}
+
+interface Produto {
+  imagem: string;
+  nome: string;
+  categoria: string;
+}
+
 @Component({
   selector: 'app-home',
   imports: [RouterLink, CommonModule],
@@ -29,5 +41,34 @@ export class Home {
     this.btnHomeTexto = 'Solicitar Orçamento'; 
   }
     
-  
+  public servicos: Servico[] = [
+    {
+      icone: 'M12 2v20M2 12h20',
+      titulo: 'Diagnóstico',
+      descricao: 'Avaliação...',
+    },
+    {
+      icone: 'M4 4h16v16H4z',
+      titulo: 'Manutenção ...',
+      descricao: 'Cuidados...',
+    },
+    {
+      icone: 'M12 2l9 4v6c0 5-3.8 9.4-9 10-5.2-.6-9-5-9-10V6l9-4z',
+      titulo: 'Manutenção ...',
+      descricao: 'Conserto rápido...',
+    },
+    {
+      icone: 'M12 20l9-16H3l9 16z',
+      titulo: 'Acompanhamento Online',
+      descricao: 'Acompanhe o orçamento...',
+    },
+  ];
+
+  //SEÇÃO PRODUTOS/EQUIPAMENTOS EM DESTAQUE
+  public produtos: Produto[] = [
+    { imagem: 'images/notebook.png', nome: 'Notebooks', categoria: 'Informática' },
+    { imagem: 'impressora.png', nome: 'Impressoras', categoria: 'Escritório' },
+    { imagem: 'eletrodomestico.png', nome: 'Eletrodomésticos', categoria: 'Casa' },
+    { imagem: 'celular.png', nome: 'Celulares', categoria: 'Mobile' },
+  ];
 }
