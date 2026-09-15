500c2141bf1e1592592de486ed33971c0c1d5956
Daniela Cristina Amorim de Souza <danitina.souza@gmail.com>
2026-09-15 00:58:25 -0300

    alterando nome e logo das páginas

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -44,7 +44,7 @@ export class Home {
   public servicos: Servico[] = [
     {
       icone: 'M22 12h-2.48a2 2 0 0 0-1.93 1.46l-2.35 8.36a.25.25 0 0 1-.48 0L9.24 2.18a.25.25 0 0 0-.48 0l-2.35 8.36A2 2 0 0 1 4.49 12H2',
-      titulo: 'Diagnóstico',
+      titulo: 'Avaliação Inicial',
       descricao: 'Avaliação tecnica completa para identificar a causa do problema',
     },
     {
